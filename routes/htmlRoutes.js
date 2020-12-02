f084705c770246824b099226ca406e1e161899ec
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-12-01 20:37:38 -0800

    added html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,19 @@
-const router = require("express")
-const Workout = require("../workoutModel.js")
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
-app.get("/", function (req, res){
-    res.sendFile("index.html");
-})
+const app = express;
+
+module.exports = () => {
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "./public/index.html"))    
+    });
+
+    app.get("/exercise", (req,res) => {
+        res.sendFile(path.join(__dirname, "./public/exercise.html"))
+    });
+    
+    app.get("/stats", (req,res) => {
+        res.sendFile(path.join(__dirname, "./public/stats.html"))
+    })
+}
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post("/submit", ({ body }, res) => {
     });
 });
 
-
+app.use(require("./routes/htmlRoutes.js"))
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
