a40f853d55db33948ad0e436766f1f5e2ca155da
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-12-03 10:01:09 -0800

    pushing to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+//port
 const PORT = process.env.PORT || 3000;
 
 const Workout = require("./models/workoutModel.js");
