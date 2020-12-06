7f6c004b6e54696140a633fa13d8564c64da52c3
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-12-05 18:33:44 -0800

    fixing atlas

--- server.js
+++ server.js
@@ -14,17 +14,26 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //connect to DB
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    // useUnifiedTopology: true,
-    // useCreateIndex: true,
-    useFindAndModify: false
-  // }).then(()=> console.log("connected to Mongodb"))
-  // .catch((err) => console.log(err)
-  });
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/workout',
+//   {
+//     useNewUrlParser: true,
+//     // useUnifiedTopology: true,
+//     // useCreateIndex: true,
+//     useFindAndModify: false
+//   // }).then(()=> console.log("connected to Mongodb"))
+//   // .catch((err) => console.log(err)
+//   });
 
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = "mongodb+srv://jesusgon4554:Sprinkles*54@cluster0.uezxk.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+  });
+  
 //routes
 app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes"));
